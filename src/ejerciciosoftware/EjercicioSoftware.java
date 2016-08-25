f9d9e992702e477a3902610c57e397a011248ca9
Usuario <Usuario@Usuario-PC>
2016-08-25 15:33:21 -0500

    hola mundo

---
+++ src/ejerciciosoftware/EjercicioSoftware.java
@@ -0,0 +1,23 @@
+
+package ejerciciosoftware;
+
+/**
+ *Angela margarita morante moreira 
+ */
+public class EjercicioSoftware {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        
+    System.out.println("hola mundo");
+    
+
+    }
+
+    
+            
+    
+}
